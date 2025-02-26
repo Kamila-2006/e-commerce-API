a06e0f561f708f3577ec7f5c14512c9ef4ce6cac
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-26 23:14:24 +0500

    models created

--- orders/models.py
+++ orders/models.py
@@ -5,22 +5,39 @@ from products.models import Product
 class Order(models.Model):
 
     STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('processing', 'Processing'),
-        ('shipped', 'Shipped'),
-        ('delivered', 'Delivered'),
-        ('cancelled', 'Cancelled'),
+        ('pending', 'В ожидании'),
+        ('processing', 'Сборка'),
+        ('shipped', 'Отправлен'),
+        ('delivered', 'Доставлен'),
+        ('cancelled', 'Отменён'),
     ]
 
     customer_name = models.CharField(max_length=100)
     customer_email = models.EmailField()
     customer_phone = models.CharField(max_length=15)
-    total_price = models.DecimalField(max_digits=7, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateField(auto_now_add=True)
+
+    @property
+    def total_price(self):
+        return sum(item.calculate_total() for item in self.items.all())
+
+    def __str__(self):
+        return f"Заказ №{self.id} - {self.customer_name}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def calculate_total(self):
+        return self.price * self.quantity
+
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.price = self.product.price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity} штук"
